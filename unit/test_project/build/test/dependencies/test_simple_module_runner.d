f5a28cd43071a27e80a57bd9cf46b1a23bc49421
shehab tarek <shehab162519@gmail.com>
2024-09-13 22:10:58 +0300

    delete build

--- unit/test_project/build/test/dependencies/test_simple_module_runner.d
+++
@@ -1,4 +0,0 @@
-build/test/out/c/test_simple_module_runner.o: \
- build/test/runners/test_simple_module_runner.c \
- C:/Ruby30-x64/lib/ruby/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h \
- C:/Ruby30-x64/lib/ruby/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity_internals.h
