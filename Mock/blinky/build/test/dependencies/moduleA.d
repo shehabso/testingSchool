ac2e0aa90be151daf8a331159961ce2d1a92b0c1
shehab tarek <shehab162519@gmail.com>
2024-09-13 20:17:34 +0300

    deleted

--- Mock/blinky/build/test/dependencies/moduleA.d
+++
@@ -1,1 +0,0 @@
-build/test/out/c/moduleA.o: src/moduleA.c src/moduleA.h
--- Mock/blinky/build/test/dependencies/test_moduleA_runner.d
+++
@@ -1,4 +0,0 @@
-build/test/out/c/test_moduleA_runner.o: \
- build/test/runners/test_moduleA_runner.c \
- C:/Ruby30-x64/lib/ruby/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h \
- C:/Ruby30-x64/lib/ruby/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity_internals.h
