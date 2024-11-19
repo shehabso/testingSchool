6122f00486b07fa24900ea31600081503bab6816
shehab tarek <shehab162519@gmail.com>
2024-11-19 22:10:30 +0200

    2

---
+++ ceedling-cppcheck-example/src/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int MAIN(void)
+{
+	char *str = "Non-compliant";
+	
+	return 0;
+}
--- ceedling-cppcheck-example/test_project/build/test/preprocess/includes/test_simple_module.c
+++
@@ -1,3 +0,0 @@
----
-- C:/Ruby30-x64/lib/ruby/gems/3.0.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h
-- src/simple_module.h
